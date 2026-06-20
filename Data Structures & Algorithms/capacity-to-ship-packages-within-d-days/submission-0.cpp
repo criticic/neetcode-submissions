63d218c5ee6501208316d807b158de49fcb3006e
Sagnik Mandal <acriticalcynic@outlook.com>
2026-06-20 13:52:24 +0530

    Add: capacity-to-ship-packages-within-d-days - submission-0

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-0.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    bool check(vector<int>& weights, int days, int capacity) {
+        int neededDays = 1;
+        int currentLoad = 0;
+
+        for (int weight : weights) {
+            if (weight > capacity) {
+                return false;
+            }
+
+            if (currentLoad + weight <= capacity) {
+                currentLoad += weight;
+            } else {
+                neededDays++;
+                currentLoad = weight;
+            }
+        }
+
+        return neededDays <= days;
+    }
+
+   public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int lo = 0;
+        int hi = std::accumulate(weights.begin(), weights.end(), 0);
+
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (check(weights, days, mid)) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+        return lo;
+    }
+};
